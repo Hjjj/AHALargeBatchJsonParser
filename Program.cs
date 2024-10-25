204e493a08fa938f2e4106ac7c4fed15bdc58784
Hjjj <wenonaliar@yahoo.com>
2024-10-25 14:55:29 -0700

    the version that has serilog, sqlite building but not tested.

--- Program.cs
+++ Program.cs
@@ -1,10 +1,9 @@
 ﻿using System;
 using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+using System.Configuration;
 using System.Data.SQLite;
 using System.IO;
+using Serilog;
 
 namespace AhaLargeBatchParser
 {
@@ -15,57 +14,89 @@ namespace AhaLargeBatchParser
     /// </summary>
     class Program
     {
-        const string JSON_FILES_TO_PROCESS_TABLE = "FilePaths";
+        const string JSON_FILES_TO_PROCESS_TABLE = "WorkQueue";
 
         static void Main(string[] args)
         {
-            string directoryPath = @"\\wmodfs01\it\NB_AHA_Cards\100JSON";
-            string dbPath = "filePaths.db";
+            string directoryPath = ConfigurationManager.AppSettings["DirectoryPath"];
+            string dbPath = ConfigurationManager.AppSettings["DbPath"];
+            string csvFolder = ConfigurationManager.AppSettings["CsvFolder"];
+            string logFolder = ConfigurationManager.AppSettings["LogFolder"];
 
-            if (!File.Exists(dbPath))
-            {
-                Console.WriteLine($"DB not found at {dbPath}");
-            }
+            string rootFolder = AppDomain.CurrentDomain.BaseDirectory;
+            InitializePaths(dbPath, csvFolder, logFolder);
 
-            using (var connection = new SQLiteConnection($"Data Source={dbPath};Version=3;"))
-            {
-                connection.Open();
+            // Configure Serilog
+            Log.Logger = new LoggerConfiguration()
+                .WriteTo.File(Path.Combine(logFolder, "log.txt"))
+                .CreateLogger();
 
-                // Create table if it doesn't exist
-                string createTableQuery = $"CREATE TABLE IF NOT EXISTS {JSON_FILES_TO_PROCESS_TABLE} (Id INTEGER PRIMARY KEY, Path TEXT)";
-                using (var command = new SQLiteCommand(createTableQuery, connection))
-                {
-                    command.ExecuteNonQuery();
-                }
-
-                int ct = GetRowCount(dbPath);
-
-                if(ct == 0)
-                {
-                    // Loop through files in the directory
-                    foreach (var filePath in Directory.GetFiles(directoryPath))
-                    {
-                        string insertQuery = $"INSERT INTO {JSON_FILES_TO_PROCESS_TABLE} (Path) VALUES (@Path)";
-                        using (var command = new SQLiteCommand(insertQuery, connection))
-                        {
-                            command.Parameters.AddWithValue("@Path", filePath);
-                            command.ExecuteNonQuery();
-                        }
-                    }
-                }
-
-                connection.Close();
-            }
-
-            Console.WriteLine("File paths have been added to the database.");
+            Log.Information("Application started.");
 
             var filePaths = GetAllFilePaths(dbPath);
-            Console.WriteLine($"There are {filePaths.Count} file paths in the database.");
+            Log.Information($"There are {filePaths.Count} file paths in the Work Queue.");
 
             int rowCount = GetRowCount(dbPath);
-            Console.WriteLine($"There are {rowCount} rows in the database.");
+            Log.Information($"There are {rowCount} rows in the database.");
 
+            Log.Information("Application finished.");
             Console.ReadKey();
+        }
+
+        static void InitializePaths(string dbPath, string csvFolder, string logFolder)
+        {
+            string rootFolder = AppDomain.CurrentDomain.BaseDirectory;
+
+            //is the db already there
+            if (!File.Exists(dbPath))
+            {
+                Console.WriteLine($"y|n, Create new database at {Path.Combine(rootFolder, dbPath)}");
+                ConsoleKeyInfo response = Console.ReadKey();
+                if (response.KeyChar == 'y')
+                {
+                    using (var connection = new SQLiteConnection($"Data Source={dbPath};Version=3;"))
+                    {
+                        connection.Open();
+
+                        // Create table if it doesn't exist
+                        string createTableQuery = $"CREATE TABLE IF NOT EXISTS {JSON_FILES_TO_PROCESS_TABLE} (Id INTEGER PRIMARY KEY, Path TEXT, IsComplete INTEGER, Comments TEXT)";
+                        using (var command = new SQLiteCommand(createTableQuery, connection))
+                        {
+                            command.ExecuteNonQuery();
+                        }
+
+                        connection.Close();
+                    }
+                    Console.WriteLine($"DB Created.");
+                }
+                else
+                {
+                    Console.WriteLine("Exiting program.");
+                    Environment.Exit(0);
+                }
+            }
+
+            if (csvFolder == string.Empty)
+            {
+                csvFolder = rootFolder;
+            }
+            else if (!Directory.Exists(csvFolder))
+            {
+                Console.WriteLine($"CSV folder does not exist. Any key to exit.");
+                Console.ReadKey();
+                Environment.Exit(0);
+            }
+
+            if (logFolder == string.Empty)
+            {
+                logFolder = rootFolder;
+            }
+            else if (!Directory.Exists(logFolder))
+            {
+                Console.WriteLine($"Log folder does not exist. Any key to exit.");
+                Console.ReadKey();
+                Environment.Exit(0);
+            }
         }
 
         static int GetRowCount(string dbPath)
@@ -114,5 +145,9 @@ namespace AhaLargeBatchParser
             return filePaths;
         }
 
+        static bool IsFullFolderPath(string fileName)
+        {
+            return Path.GetDirectoryName(fileName) != string.Empty;
+        }
     }
 }
